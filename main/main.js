50288690116f4d7c7734497555b93ae92dc971e0
Yakshit <92084737+YAKSHIT-22@users.noreply.github.com>
2023-01-20 00:41:45 +0530

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,40 +1,54 @@
-const head = document.getElementsByTagName("HEAD")[0];
-const tagList = [
-  "a",
-  "p",
-  "div",
-  "span",
-  "li",
-  "button",
-  "input",
-  "lable",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "form"
-];
-const fontslist = [];
-window.addEventListener("load", () => {
-  tagList.forEach((tag) => {
-    let allclass = document.querySelectorAll(tag);
-    for (let i = 0; i < allclass.length; i++) {
-      let singleClass = allclass[i];
-      if (singleClass.className.includes("brahmastra-")) {
-        let font = singleClass.className.split("-")[1];
-        let fontsize = singleClass.className.split("-")[2];
-        if (!fontslist.includes(font)) {
-            fontslist.push(font);
-            let link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = `https://fonts.googleapis.com/css2?family=${font}&display=swap`;
-            head.appendChild(link);
+ var x = document.getElementsByTagName("HEAD")[0];
+
+    const tag = [
+      "a",
+      "p",
+      "div",
+      "span",
+      "li",
+      "button",
+      "input",
+      "lable",
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "form",
+    ];
+    const fontslist = [];
+
+    window.addEventListener("load", () => {
+      tag.forEach((tag) => {
+        let allclass = document.querySelectorAll(tag);
+
+        for (let i = 0; i < allclass.length; i++) {
+          let singleClass = allclass[i];
+
+          if (singleClass.className.includes("brahmastra-")) {
+            let font = singleClass.classList;
+            let font1 = null;
+            let fontsize = null;
+            font.forEach((i) => {
+              if (i.split("-")[0] === "brahmastra") {
+                font1 = i.split("-")[1];
+                fontsize = i.split("-")[2];
+              }
+            });
+
+            if (!fontslist.includes(font1)) {
+              fontslist.push(font1);
+              console.log(fontslist);
+              let link = document.createElement("link");
+              link.rel = "stylesheet";
+              link.href = `https://fonts.googleapis.com/css2?family=${font1}&display=swap`;
+              head.appendChild(link);
+              console.log(link);
+            }
+            singleClass.style.fontFamily = font1.replaceAll("+", " ");
+            singleClass.style.fontSize = (fontsize || "16") + "px";
+          }
         }
-        singleClass.style.fontFamily = font.replaceAll("+", " ");
-        singleClass.style.fontSize = (fontsize || "16") + "px";
-      }
-    }
-  });
-});
+      });
+    });
